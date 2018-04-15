0d2c6ac21c8e324c70b7f941b249b5eeea2c5c95
Opeezy <copenshaw17@gmail.com>
2018-04-15 16:21:54 -0500

    even more syntax errors

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('https://pacific-harbor-90607.herokuapp.com//signin', {
+		fetch('https://pacific-harbor-90607.herokuapp.com/signin', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
